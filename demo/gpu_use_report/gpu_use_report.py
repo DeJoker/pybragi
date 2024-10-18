b056931e934a0a779da38b97b04164213630514a
cxj <localjie@163.com>
2024-10-18 17:34:49 +0800

    add gpu report demo

---
+++ demo/gpu_use_report/gpu_use_report.py
@@ -0,0 +1,123 @@
+import os
+import requests
+from pybragi.base import log
+import logging
+from pydantic import BaseModel
+from sortedcontainers import SortedDict
+from pybragi.base import gpu_detect
+
+import time
+import hmac
+import hashlib
+import base64
+import urllib.parse
+
+machines = [
+    "beijing-aigc-gpt-gpu01",
+    "beijing-aigc-gpt-gpu02",
+    "beijing-aigc-gpt-gpu03",
+    "beijing-aigc-gpt-gpu07",
+    "xljn-aigc-gpt-t01002",
+    "xljn-aigc-gpt-t01003",
+    "shal-zy-aigc-gpt-gpu01",
+]
+
+class MetricsStat(BaseModel):
+    Gpu: str = ""
+    # 以下为90时间分位的数值 而不是数值vector的90分位
+    UtiltyAvg: float = 0.0
+    MemoryAvg: float = 0.0
+    MemoryP50: float = 0.0
+
+    def pretty_print(self):
+        return f"gpu:{self.Gpu} 平均使用率:{self.UtiltyAvg:.3f}% " +\
+                f"平均显存占用:{self.MemoryAvg/1024:.3f}GB 50时间分位显存占用:{self.MemoryP50/1024:.3f}GB"
+
+
+def aliding_host():
+    timestamp = str(round(time.time() * 1000))
+    DING_SECRET = os.environ.get("DING_SECRET", "")
+    secret_enc = DING_SECRET.encode('utf-8')
+    string_to_sign = '{}\n{}'.format(timestamp, DING_SECRET)
+    string_to_sign_enc = string_to_sign.encode('utf-8')
+    hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
+    sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
+
+    DING_ACCESS = os.environ.get("DING_ACCESS", "")
+    url = f"https://oapi.dingtalk.com/robot/send?access_token={DING_ACCESS}" +\
+          f"&timestamp={timestamp}&sign={sign}"
+    logging.info(url)
+    return url
+
+
+def dingcall(title, text):
+    data = {
+        "msgtype": "markdown",
+        "markdown": {
+            "title": title,
+            "text": text
+        },
+        "at": {
+            # "atMobiles": at_list, "isAtAll": isAtAll
+        },
+    }
+    url = aliding_host()
+
+    headers = {'Content-Type': 'application/json;charset=UTF-8'}
+    result = requests.post(url, json=data, headers=headers)
+    if result.json()['errcode'] == 0:
+        print('发送成功')
+    else:
+        print('发送失败')
+
+
+
+def report_all():
+    for machine in machines:
+        uitlity_avg = f'avg_over_time(DCGM_FI_DEV_GPU_UTIL{{Hostname="{machine}"}}[7d])'
+        memory_avg = f'avg_over_time(DCGM_FI_DEV_FB_USED{{Hostname="{machine}"}}[7d])'
+        memory_p90 = f'quantile_over_time(0.5, DCGM_FI_DEV_FB_USED{{Hostname="{machine}"}}[7d])'
+
+        if machine.startswith("shal") or machine.startswith("xljn"):
+            uitlity_avg = f'avg_over_time(DCGM_FI_DEV_GPU_UTIL{{host="{machine}"}}[7d])'
+            memory_avg = f'avg_over_time(DCGM_FI_DEV_FB_USED{{host="{machine}"}}[7d])'
+            memory_p90 = f'quantile_over_time(0.5, DCGM_FI_DEV_FB_USED{{host="{machine}"}}[7d])'
+
+
+        current = SortedDict()
+        res = gpu_detect.get_gpu_utilty_prometheus(uitlity_avg)
+        result = res.get("data", {}).get("result", [])
+        for res in result:
+            gpu = res.get("metric", {}).get("gpu", -1)
+            uitlityAvg = float(res.get("value", [0, ''])[1])
+            current[gpu] = MetricsStat(Gpu=gpu, UtiltyAvg=uitlityAvg)
+        
+        res = gpu_detect.get_gpu_utilty_prometheus(memory_avg)
+        result = res.get("data", {}).get("result", [])
+        for res in result:
+            gpu = res.get("metric", {}).get("gpu", -1)
+            memoryAvg = float(res.get("value", [0, ''])[1])
+            current[gpu].MemoryAvg = memoryAvg
+        
+        res = gpu_detect.get_gpu_utilty_prometheus(memory_p90)
+        result = res.get("data", {}).get("result", [])
+        for res in result:
+            gpu = res.get("metric", {}).get("gpu", -1)
+            memoryP50 = float(res.get("value", [0, ''])[1])
+            current[gpu].MemoryP50 = memoryP50
+        
+        logging.info(f"{machine}")
+        dingmsg = f"## {machine} 资源使用情况 \n"
+        for _,stat in current.items():
+            logging.info(f"{stat.pretty_print()}")
+            dingmsg += f"> {stat.pretty_print()}  \n"
+        dingcall(machine, dingmsg)
+
+
+
+if __name__ == "__main__":
+    text = "# 一级标题 \n" + "## 二级标题 \n" + "> 引用文本  \n" + "**加粗**  \n" + "*斜体*  \n" + "<font color=\"#FF0000\">颜色</font> \n" + "[百度链接](https://www.baidu.com)\n\n\n\n"
+    # dingcall("过去七天使用情况", text)
+
+    report_all()
+
--- pybragi/base/gpu_detect.py
+++ pybragi/base/gpu_detect.py
@@ -1,11 +1,9 @@
-import threading
+import json
 import time
 import pynvml # pip install nvidia-ml-py
 import logging
 from collections import deque
-
 import requests
-from responses import target
 
 def get_free_memory():
     free_mb = []
@@ -29,13 +27,46 @@ def valid_memory_card(mb: int):
             return i, free_mb
     return -1, free_mb
 
+def valid_memory_cards(mb: int):
+    cards_tuple = []
+    free_mb = get_free_memory()
+    for i, free in enumerate(free_mb):
+        if free > mb:
+            cards_tuple.append((i, free))
+    return cards_tuple
 
-def record_gpu_utilty():
-    time_range = 3 * 60  # 5 minutes
-    sample_interval = 3  # 5 seconds
+def continuous_valid_cards(mb=0, seq=2):
+    items = valid_memory_cards(mb)
+    cards, memroys = zip(*items)
+
+    for i in range(len(cards)):
+        if cards[i+seq-1] == cards[i]+seq-1:
+            return cards[i:i+seq]
+    return []
+
+def valid_cards(mb=0, num=2):
+    items = valid_memory_cards(mb)
+    cards, memroys = zip(*items)
+
+    if len(cards) >= num:
+        return cards[:num]
+    return []
+
+
+gpu_util_deques = []
+def gpu_util_queue(deviceCount=8, time_range=5*60, sample_interval=2):
+    global gpu_util_deques
+    if not gpu_util_deques:
+        # gpu_util_deques = [deque(maxlen=time_range // sample_interval)] * deviceCount # 浅拷贝实际上是引用
+        gpu_util_deques = [deque(maxlen=time_range // sample_interval) for _ in range(deviceCount)]  # 深拷贝 独立对象
+    return gpu_util_deques
+
+
+def record_gpu_utilty(sample_interval=2):
     pynvml.nvmlInit()
     deviceCount = pynvml.nvmlDeviceGetCount()
-    gpu_util_deques = [deque(maxlen=time_range // sample_interval)] * deviceCount
+    gpu_util_deques = gpu_util_queue(deviceCount=deviceCount)
+    print()
 
     while True:
         try:
@@ -46,27 +77,22 @@ def record_gpu_utilty():
                 gpu_util_deques[i].append(util_rate.gpu)
 
                 average_gpu_util = sum(gpu_util_deques[i]) / len(gpu_util_deques[i])
-                print(f"Average GPU Utilization {i} over the last {len(gpu_util_deques[i])*3 / 60} minutes: {average_gpu_util:.2f}%")
+                # print(f"Average GPU Utilization {i} over the last {len(gpu_util_deques[i])*sample_interval/60:.2f} minutes: {average_gpu_util:.2f}%")
 
-                time.sleep(sample_interval)
+            time.sleep(sample_interval)
 
         except KeyboardInterrupt:
             print("Stopped by User")
+        finally:
+            pass
 
     pynvml.nvmlShutdown()
 
 
-threading.Thread(target=record_gpu_utilty, daemon=True).start()
-time.sleep(9)
-
-def get_gpu_utilty_prometheus():
+def get_gpu_utilty_prometheus(query = f'avg_over_time(DCGM_FI_DEV_GPU_UTIL{{Hostname="beijing-aigc-gpt-gpu02"}}[7d])'):
     PROMETHEUS = "http://192.168.220.223:9090"
     end_time = int(time.time())
-    # 注释掉的部分返回的是每个时间点的结果
-    # query = f'avg_over_time(DCGM_FI_DEV_GPU_UTIL{{host="beijing-aigc-gpt-gpu02"}}[{start_time}s:{end_time}s])'
-    query = f'avg_over_time(DCGM_FI_DEV_GPU_UTIL{{host="beijing-aigc-gpt-gpu02"}}[5m])'
 
-    # url = f"{PROMETHEUS}/api/v1/query_range"
     url = f"{PROMETHEUS}/api/v1/query"
     params = {
         "query": query,
@@ -79,14 +105,34 @@ def get_gpu_utilty_prometheus():
     # 发送请求
     response = requests.get(url, params=params)
     if response.ok:
-        logging.info(f"{response.url} {response.text}")
+        logging.debug(f"{response.url} {response.text}")
+        xx = json.loads(response.text)
+        return xx
     else:
-        logging.info(f"{response.url} {response.status_code} {response.text}")
+        logging.error(f"{response.url} {response.status_code} {response.text}")
 
 if __name__ == '__main__':
     from service.base import log
-    get_gpu_utilty_prometheus()
+
+    def test_local_detect():
+        x = continuous_valid_cards(78000, 2)
+        logging.info(x)
+        x = continuous_valid_cards(78000, 3)
+        logging.info(x)
+
+        import threading
+        threading.Thread(target=record_gpu_utilty, daemon=True).start()
+        time.sleep(9)
+
+        # get_gpu_utilty_prometheus()
+        
+        print("asdas")
+        time.sleep(1)
+        print("asdas end")
     
-    print("asdas")
-    time.sleep(1)
-    print("asdas end")
+    def test_prometheus():
+        get_gpu_utilty_prometheus('histogram_quantile(0.5, sum(rate(DCGM_FI_DEV_GPU_UTIL{Hostname="beijing-aigc-gpt-gpu02"}[7d])) by (gpu, le))')
+    
+
+    test_prometheus()
+        
