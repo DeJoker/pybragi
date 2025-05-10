b525f524eb20149c034b06341bf8a2c628a3e876
崔勇杰 <cuiyongjie2014@xiaochuankeji.cn>
2025-05-10 11:29:25 +0800

    add health api

--- pybragi/base/base_handler.py
+++ pybragi/base/base_handler.py
@@ -1,4 +1,6 @@
+import signal
 from pybragi.base import log
+import logging
 from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor
 from tornado import web, gen, ioloop
@@ -20,19 +22,21 @@ class Echo(metrics.PrometheusMixIn):
 
 
 class HealthCheckHandler(metrics.PrometheusMixIn):
-    executor = ThreadPoolExecutor(1)
+    executor = ThreadPoolExecutor(5)
 
     # https://www.tornadoweb.org/en/stable/web.html#tornado.web.RequestHandler.initialize
     def initialize(self, name=""):
         self.name = name
 
     def _log(self):
+        # logging.info(f"{self.request.request_time()}")
+        if self.request.request_time() > 0.002:
+            super()._log()
         return
 
     def log_request(self):
         return
 
-    @run_on_executor
     def current(self):
         now = datetime.now()
         res = {
@@ -41,19 +45,20 @@ class HealthCheckHandler(metrics.PrometheusMixIn):
             "data": {
                 "name": self.name,
                 "timestamp": int(now.timestamp()),
-                "timestamp-str": now.strftime("%Y-%m-%d %H:%M:%S"),
+                "timestamp-str": now.strftime("%Y-%m-%d %H:%M:%S.%f"),
             },
         }
         return res
 
-    @gen.coroutine
+    @run_on_executor
     def get(self):
-        res = yield self.current()
+        res = self.current()
         self.write(res)
 
-    @gen.coroutine
+    @run_on_executor
     def post(self):
-        self.get()
+        res = self.current()
+        self.write(res)
 
 class CORSBaseHandler(web.RequestHandler):
     def set_default_headers(self):
@@ -73,6 +78,7 @@ def make_tornado_web(service: str, big_latency=False, kafka=False):
         [
             (r"/echo", Echo),
             (r"/healthcheck", HealthCheckHandler, dict(name=service)),
+            (r"/health", HealthCheckHandler, dict(name=service)),
             (r"/metrics", metrics.MetricsHandler),
         ]
     )
@@ -85,8 +91,22 @@ def run_tornado_app(app: web.Application, port=8888):
     ioloop.IOLoop.current().start()
 
 
+def base_handle_exit_signal(signum, frame):
+    logging.info("Received exit signal. Setting exit event.")
+    tornado_ioloop = ioloop.IOLoop.current()
+    tornado_ioloop.add_callback_from_signal(tornado_ioloop.stop)
+
+
+
 # python -m service.base.base_handler
 if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", type=int, default=8888)
+    args = parser.parse_args()
+    
+    signal.signal(signal.SIGINT, base_handle_exit_signal)
+    signal.signal(signal.SIGTERM, base_handle_exit_signal)
     app = make_tornado_web(__file__)
-    run_tornado_app(app)
+    run_tornado_app(app, args.port)
 
